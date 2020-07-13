812464e04f5e5018e39a89013afd543647f5ddc9
Dyarlen Iber <dyarlen1@gmail.com>
2020-07-13 22:00:29 +0100

    Update Favorite page tests

--- src/__tests__/pages/Favorites.test.js
+++ src/__tests__/pages/Favorites.test.js
@@ -6,15 +6,43 @@ import configureStore from 'redux-mock-store';
 import Favorites from '../../pages/Favorites';
 
 describe('Favorites page', () => {
-  const middlewares = [];
-  const mockStore = configureStore(middlewares);
-  const initialState = {
-    favorites: [],
-  };
-  const store = mockStore(initialState);
-
   it('should render without crashing', () => {
-    const wrapper = mount(
+    let middlewares;
+    let mockStore;
+    let initialState;
+    let store;
+    let wrapper;
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      favorites: [],
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <Favorites />
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      favorites: [
+        {
+          display_title: 'title',
+          summary_short: 'description',
+          publication_date: '01-01-2020',
+          multimedia: { src: '#' },
+          link: { url: '#' },
+          byline: 'criticName',
+          critics_pick: 1,
+        },
+      ],
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
       <Provider store={store}>
         <Favorites />
       </Provider>
