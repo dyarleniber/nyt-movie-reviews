0b7c465070caa6751f6b2fb33fa601bd79f50a4b
Dyarlen Iber <dyarlen1@gmail.com>
2020-07-13 21:41:30 +0100

    Update Critics page tests

--- src/__tests__/pages/Critics.test.js
+++ src/__tests__/pages/Critics.test.js
@@ -2,24 +2,106 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { MemoryRouter } from 'react-router-dom';
 
 import Critics from '../../pages/Critics';
 
 describe('Critics page', () => {
-  const middlewares = [];
-  const mockStore = configureStore(middlewares);
-  const initialState = {
-    critics: {
-      loading: false,
-      critics: [],
-    },
-  };
-  const store = mockStore(initialState);
-
   it('should render without crashing', () => {
-    const wrapper = mount(
+    let middlewares;
+    let mockStore;
+    let initialState;
+    let store;
+    let wrapper;
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      critics: {
+        loading: true,
+        critics: [],
+      },
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
       <Provider store={store}>
-        <Critics />
+        <MemoryRouter
+          initialEntries={[{ pathname: '/critics', key: 'testKey' }]}
+        >
+          <Critics />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      critics: {
+        loading: false,
+        critics: [],
+      },
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: '/critics', key: 'testKey' }]}
+        >
+          <Critics />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      critics: {
+        loading: false,
+        critics: [
+          {
+            multimedia: { resource: { src: '#' } },
+            display_name: 'name',
+            bio: 'description',
+          },
+        ],
+      },
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: '/critics', key: 'testKey' }]}
+        >
+          <Critics />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+    middlewares = [];
+    mockStore = configureStore(middlewares);
+    initialState = {
+      critics: {
+        loading: false,
+        critics: [
+          {
+            multimedia: null,
+            display_name: 'name 2',
+            bio: 'description',
+          },
+        ],
+      },
+    };
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: '/critics', key: 'testKey' }]}
+        >
+          <Critics />
+        </MemoryRouter>
       </Provider>
     );
     expect(wrapper).toMatchSnapshot();
