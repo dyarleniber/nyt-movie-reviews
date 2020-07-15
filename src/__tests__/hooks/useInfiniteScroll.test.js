23c85a7885224c225ea228fd537e3b7b084d4a23
Dyarlen Iber <dyarlen1@gmail.com>
2020-07-15 20:01:35 -0300

    useInfiniteScroll hook test

---
+++ src/__tests__/hooks/useInfiniteScroll.test.js
@@ -0,0 +1,38 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
+
+describe('useInfiniteScroll hook', () => {
+  const originalInnerHeight = window.innerHeight;
+
+  afterEach(() => {
+    window.innerHeight = originalInnerHeight;
+
+    jest.restoreAllMocks();
+  });
+
+  it('should add scroll event listener', () => {
+    delete window.innerHeight;
+    window.innerHeight = 0;
+
+    const events = {};
+
+    jest
+      .spyOn(window, 'addEventListener')
+      .mockImplementation((event, handle) => {
+        events[event] = handle;
+      });
+
+    const callback = jest.fn();
+
+    renderHook(() => useInfiniteScroll(callback));
+
+    expect(window.addEventListener).toHaveBeenCalledWith(
+      'scroll',
+      expect.any(Function)
+    );
+
+    events.scroll();
+
+    expect(callback).toHaveBeenCalled();
+  });
+});
